5276b1e6c8ba2fa5eb29dd6f5453911e1f361956
henglysok <sokhenglystudent@gmail.com>
2025-02-07 14:08:10 +0700

    created repository to githab

---
+++ index.js
@@ -0,0 +1,9 @@
+var numberofDrumButtons = document.querySelectorAll(".drum").length;
+
+for (var i =0; i < numberofDrumButtons; i ++ ) {
+    document.querySelectorAll(".drum")[i].addEventListener('click', 
+    function(){
+        var audio = new Audio('sounds/tom-1.mp3');
+        audio.play();
+    });
+}
