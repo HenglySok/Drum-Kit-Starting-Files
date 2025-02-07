e5405bb959422196769de1bf1d0320d8d08d9226
henglysok <sokhenglystudent@gmail.com>
2025-02-07 21:31:23 +0700

    updated

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ var numberofDrumButtons = document.querySelectorAll(".drum").length;
 for (var i =0; i < numberofDrumButtons; i ++ ) {
     document.querySelectorAll(".drum")[i].addEventListener('click', 
     function(){
-        var audio = new Audio('sounds/tom-1.mp3');
-        audio.play();
+        this.style.color = "white";
     });
 }
+
+
+// var audio = new Audio('sounds/tom-1.mp3'); // mySound = new Audio([URLString]);
+//         audio.play();
